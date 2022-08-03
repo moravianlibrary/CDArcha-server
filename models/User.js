961a20596fccf2a1f216d913b12ef773d738b73d
lubo <orsag@cosmotron.cz>
2022-08-03 15:01:21 +0200

    Oprava issue #28

--- models/User.js
+++ models/User.js
@@ -32,7 +32,7 @@ userSchema.pre('save', function save(next) {
   if (!user.isModified('password')) { return next(); }
   bcrypt.genSalt(10, (err, salt) => {
     if (err) { return next(err); }
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
       if (err) { return next(err); }
       user.password = hash;
       next();
